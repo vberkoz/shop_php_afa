63c96bdcd302d6e85b00ed92d1f7d11b417eb2c3
Vasyl Berkoz <vberkoz@gmail.com>
2020-04-13 20:07:52 +0200

    Site menu

--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -1,5 +1,23 @@
 <?php
 
+function getChildrenForCat($catId, $db) {
+    $sql = "SELECT *
+            FROM categories
+            WHERE parent_id = '{$catId}'";
+
+    $rs = $db->query($sql);
+
+    $i = 0;
+    $categories = array();
+    while ($row = $rs->fetch()) {
+        $categories[$i]['id'] = $row['id'];
+        $categories[$i]['parent_id'] = $row['parent_id'];
+        $categories[$i]['name'] = $row['name'];
+        $i++;
+    }
+    return $categories;
+}
+
 function getAllMainCatsWithChildren($db) {
     $sql = 'SELECT *
             FROM categories
@@ -13,6 +31,12 @@ function getAllMainCatsWithChildren($db) {
         $categories[$i]['id'] = $row['id'];
         $categories[$i]['parent_id'] = $row['parent_id'];
         $categories[$i]['name'] = $row['name'];
+
+        $rsChildren = getChildrenForCat($row['id'], $db);
+        if ($rsChildren) {
+            $categories[$i]['children'] = $rsChildren;
+        }
+
         $i++;
     }
     return $categories;
--- views/default/leftcolumn.tpl
+++ views/default/leftcolumn.tpl
@@ -1,7 +1,16 @@
 <div id="leftColumn">
-    <div id="leftMenu">
+    <ul>
         {foreach $rsCategories as $item}
-            <a href="#">{$item['name']}</a><br>
+            <li>
+                <a href="#">{$item['name']}</a>
+                {if isset($item['children'])}
+                    <ul>
+                        {foreach $item['children'] as $itemChildren}
+                            <li><a href="#">{$itemChildren['name']}</a></li>
+                        {/foreach}
+                    </ul>
+                {/if}
+            </li>
         {/foreach}
-    </div>
+    </ul>
 </div>
